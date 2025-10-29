8822b8ebc4e4fb31a62a584ed970b1bad3376136
Andrés Pinto <andres.pinto.n@gmail.com>
2025-10-29 10:38:59 -0500

    Fix mod loading: Add dedicated_server_mods_setup.lua and fix modoverrides.lua syntax + Spanish Language Pack

---
+++ docker-server/dedicated_server_mods_setup.lua
@@ -0,0 +1,13 @@
+-- Mods to download on server start
+ServerModSetup("1458450094") -- Better Traps  
+ServerModSetup("1818688368") -- Real Clock
+ServerModSetup("1974725370") -- Respawn Near
+ServerModSetup("2078243581") -- Combat Target 
+ServerModSetup("347079953")  -- Display Food Values
+ServerModSetup("358749986")  -- Player Status
+ServerModSetup("374550642")  -- Increased Stack sizes
+ServerModSetup("375859599")  -- Health Info
+ServerModSetup("458940297")  -- Display Food Values
+ServerModSetup("623749604")  -- Ice Flingomatic Range
+ServerModSetup("666155465")  -- Show Me (Display Information)
+ServerModSetup("885290954")  -- Spanish Language Pack
