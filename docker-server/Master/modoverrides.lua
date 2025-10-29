170a165ceaf6db0cb45b714287c3985654c538b7
Andrés Pinto <andres.pinto.n@gmail.com>
2025-10-29 10:25:40 -0500

    new world

--- docker-server/Master/modoverrides.lua
+++ docker-server/Master/modoverrides.lua
@@ -1,11 +1,42 @@
 return {
+  ["workshop-1458450094"]={
+    configuration_options={
+      [""]=0,
+      houndstooth=false,
+      infinite=false,
+      livinglog=false,
+      log=false,
+      mode="fix",
+      retrapfix=60,
+      retraprandomx=10,
+      retraprandomy=120,
+      rope=false,
+      stinger=false 
+    },
+    enabled=true 
+  },
   ["workshop-1818688368"]={ configuration_options={  }, enabled=true },
+  ["workshop-1974725370"]={
+    configuration_options={
+      leastnum_beefalo=2,
+      leastnum_beehive=2,
+      leastnum_catcoonden=2,
+      leastnum_knight=2,
+      leastnum_lightninggoat=2,
+      leastnum_rocky=2,
+      leastnum_slurtlehole=2,
+      leastnum_spiderden=2,
+      respawnmechanism="scripts/respawnnear.lua",
+      respawntime=5 
+    },
+    enabled=true 
+  },
   ["workshop-2078243581"]={
     configuration_options={ Blue=0, Display="target", Green=0, Projectile=true, Red=1, Type="hit" },
     enabled=true 
   },
   ["workshop-347079953"]={
-    configuration_options={ DFV_Language="SP", DFV_MinimalMode="default" },
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
     enabled=true 
   },
   ["workshop-358749986"]={
@@ -57,7 +88,7 @@ return {
       Craft="Normal",
       Destroyable="DestroyByAll",
       FoodSpoilage=1,
-      Language="Sp",
+      Language="En",
       Position="Center",
       Slots=80 
     },
@@ -77,11 +108,5 @@ return {
       show_uses=-1 
     },
     enabled=true 
-  },
-  ["workshop-885290954"]={
-    configuration_options={},
-    enabled=true 
-  },
-  -- Agregar nuevos mods aquí:
-  -- ["workshop-ID_DEL_MOD"]={ configuration_options={}, enabled=true }, 
+  } 
 }
